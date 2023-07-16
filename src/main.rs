dfefdb1c4229d76a8e829fddddd59da229866ae4
odf <olaf.delgado@gmail.com>
2023-07-16 16:21:25 +1000

    enable profiling

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,34 @@ use rust_cubes::cube_generators::Cubes;
 
 fn main() {
     if let Some(arg) = std::env::args().nth(1) {
-        generate(arg.parse().unwrap());
+        call_generate(arg.parse().unwrap());
     } else {
         panic!("Expected an argument.");
     }
+}
+
+
+
+
+#[cfg(not(feature = "pprof"))]
+fn call_generate(n: usize) {
+    generate(n);
+}
+
+
+#[cfg(feature = "pprof")]
+fn call_generate(n: usize) {
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+        .build().unwrap();
+
+    generate(n);
+
+    if let Ok(report) = guard.report().build() {
+        let file = std::fs::File::create("flamegraph.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
 }
 
 
